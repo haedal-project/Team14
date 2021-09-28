9877072de01c1958f18691bd7c150d7f6fffb74f
manijang2 <manijang2@naver.com>
2021-09-28 15:01:07 +0900

    app.py 연결 URL 매핑 수정 및 css 링크 주소 변경

---
+++ seonman-app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/seonman/')
+def main():
+    return render_template('seonman-index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
