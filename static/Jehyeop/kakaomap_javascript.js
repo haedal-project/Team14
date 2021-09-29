0d2a61f8170a7d032ebd800dca3ca1f2cbc7f2aa
Yu_Jehyeop <fbwpguq@gmail.com>
2021-09-29 22:22:17 +0900

    병합-좋아요 클릭,db_post

--- static/Jehyeop/kakaomap_javascript.js
+++ static/Jehyeop/kakaomap_javascript.js
@@ -70,8 +70,11 @@ function displayPlaces(places) {
 
     // 지도에 표시되고 있는 마커를 제거합니다
     removeMarker();
-    var positions = []
-    var addresses = []
+    let positions = []
+    let addresses = []
+    let lat = []
+    let lng = []
+
     for (var i = 0; i < places.length; i++) {
         // 마커를 생성하고 지도에 표시합니다
         var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
@@ -79,6 +82,8 @@ function displayPlaces(places) {
             itemEl = getListItem(i, places[i]), // 검색 결과 항목 Element를 생성합니다
             address = places[i].address_name;
 
+        lat.push(places[i].y);
+        lng.push(places[i].x)
         addresses.push(address);
         positions.push(placePosition);
 
@@ -107,7 +112,7 @@ function displayPlaces(places) {
             kakao.maps.event.addListener(marker, 'click', function() {
                 map.setLevel(level = 2);
                 map.setCenter(positions[num]);
-                alert(' 이름:'+title+'\n 좌표:'+positions[num]+'\n 주소'+address)
+                alert(' 이름:'+title+'\n 위도:'+lat[num]+'경도:'+lng[num]+'\n 주소'+address)
             });
 
             itemEl.onmouseover = function () {
@@ -121,7 +126,7 @@ function displayPlaces(places) {
             itemEl.onclick = function () {
                 map.setLevel(level = 2);
                 map.setCenter(positions[num]);
-                alert(' 이름:'+title+'\n 좌표:'+positions[num]+'\n 주소'+address)
+                alert(' 이름:'+title+'\n 위도:'+lat[num]+'경도:'+lng[num]+'\n 주소'+address)
             };
             itemEl.onmouseout = function () {
                 infowindow.close();
@@ -247,4 +252,68 @@ function removeAllChildNods(el) {
     while (el.hasChildNodes()) {
         el.removeChild(el.lastChild);
     }
+}
+
+
+$(document).ready(function () {
+    showbest();
+});
+
+
+function showbest() {
+    $.ajax({
+        type: 'POST',
+        url: '/api/list?sample_give=샘플데이터',
+        data: {},
+        success: function (response) {
+            let store = response['like'] //서버에서 적힌거
+            for (let i=0; i < store.length; i++){
+                // db에서 받아올 것들
+                let name = store[i]["title"]
+                let x = store[i]["x"]
+                let y = store[i]["y"]
+                let like = store[i]["like"]
+                let temp_html = `
+                            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" style="margin: 10px;">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">${name}<small class="text-muted">평점(4.5점)</small></h5>
+                                    <span><button type="button" onclick="plus('${name}')" id="like_button" class="btn btn-danger" >♡</button> + ${like}</span>
+                                </div>
+                            </a>`
+                // 마커를 표시할 위치와 title 객체 배열입니다
+                $("#hello").append(temp_html)
+
+                // 추천마커 이미지의 이미지 주소입니다
+                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+                // 마커 이미지의 이미지 크기 입니다
+                var imageSize = new kakao.maps.Size(24, 35);
+
+                // 마커 이미지를 생성합니다
+                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: new kakao.maps.LatLng(x, y),// 마커를 표시할 위치
+                    title : name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                    image : markerImage // 마커 이미지
+                });
+
+            }
+        }
+    });
+}
+
+
+function plus(name) { //이름을 받는 함수
+    $.ajax({ //like값을 올리기
+        type: 'POST',
+        url: '/api/like',
+        data: {name_give: name}, // 이름을 건네준다.
+        success: function (response) {
+            alert(response['msg']);
+            window.location.reload() // 새로고침
+        }
+    });
 }
