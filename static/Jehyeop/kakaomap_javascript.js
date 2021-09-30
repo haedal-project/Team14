a0cc40696eec05c0bf8ca75b1f9b5c7c8f9e0678
Yu_Jehyeop <fbwpguq@gmail.com>
2021-09-30 10:00:13 +0900

    수정-초기 검색값 지우기

--- static/Jehyeop/kakaomap_javascript.js
+++ static/Jehyeop/kakaomap_javascript.js
@@ -173,30 +173,6 @@ function getListItem(index, places) {
 
     return el;
 }
-//
-// // 검색결과 항목을 Element로 반환하는 함수입니다
-// function getListItem(index, places) {
-//     var el = document.createElement('li'),
-//         itemStr = '<span class="markerbg marker_' + (index + 1) + '"></span>' +
-//             '<div class="info">' +
-//             '   <h5>' + places.place_name + '</h5><br>'+'평점 :';
-//     // itemStr += '  <span class="star point">'+'평점 :'+'</span>' +
-//     //     '</div>';
-//     if (places.road_address_name) {
-//         itemStr += '    <span>' + places.road_address_name + '</span>' +
-//             '   <span class="jibun gray">' + places.address_name + '</span>';
-//     } else {
-//         itemStr += '    <span>' + places.address_name + '</span>';
-//     }
-//
-//     itemStr += '  <span class="tel">' + places.phone + '</span>' +
-//         '</div>';
-//
-//     el.innerHTML = itemStr;
-//     el.className = 'item';
-//
-//     return el;
-// }
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
 function addMarker(position, idx, title) {
     var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
