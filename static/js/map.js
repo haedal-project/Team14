25bcb30f9a6652f7dd8a7587de841a4b906c85fd
Yu_Jehyeop <fbwpguq@gmail.com>
2021-10-05 16:36:58 +0900

    추가-map.css,map.js

--- static/js/map.js
+++ static/js/map.js
@@ -17,7 +17,6 @@ let ps = new kakao.maps.services.Places();
 // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
 let infowindow = new kakao.maps.InfoWindow({zIndex: 1});
 
-
 if (count >= 1){
     searchPlaces();
 }
@@ -102,12 +101,8 @@ function displayPlaces(places) {
             kakao.maps.event.addListener(marker, 'mouseover', function () {
                 //let content = '<div style=";z-index:1;" id="info_box">' + title + '(평점:)<br>주소 '+addresses[num] +' </div>'; (수정 전 평점 지우기)
                 let content = '<div style=";z-index:1;" id="info_box">' + title + '<br>주소 '+ addresses[num] +' </div>';
-
-
-                // let content = '<div style="padding:5px;z-index:1;max-width:1200px">' + title + '(평점:)<br>주소 '+addresses[num] +' </div>';
                 infowindow.setContent(content);
                 infowindow.open(map, marker);
-                // displayInfowindow(marker, title);
             });
 
             kakao.maps.event.addListener(marker, 'mouseout', function () {
@@ -241,124 +236,4 @@ function removeAllChildNods(el) {
     while (el.hasChildNodes()) {
         el.removeChild(el.lastChild);
     }
-}
-
-
-function showbest() {
-    $.ajax({
-        type: 'GET',
-        url: '/api/recommend?sample_give=샘플데이터',
-        data: {},
-        success: function (response) {
-            let store = response['like'] //서버에서 적힌거
-            for (let i=0; i < store.length; i++){
-                // db에서 받아올 것들
-                let name = store[i]["title"]
-                let x = store[i]["x"]
-                let y = store[i]["y"]
-                let like = store[i]["like"]
-
-                let temp_html = `
-                            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start" style="margin: 10px;">
-                                <div class="d-flex w-100 justify-content-between">
-                          <h5 class="mb-1">${name}<small class="text-muted"></small></h5> <!-- 안에 평점 -->
-                                    <span><button type="button" onclick="plus('${name}')" id="like_button" class="btn btn-danger" >♡</button> + ${like}</span>
-                                </div>
-                            </a>`
-                // 마커를 표시할 위치와 title 객체 배열입니다
-                $("#hello").append(temp_html)
-
-                // 추천마커 이미지의 이미지 주소입니다
-                let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-                // 마커 이미지의 이미지 크기 입니다
-                let imageSize = new kakao.maps.Size(24, 35);
-
-                // 마커 이미지를 생성합니다
-                let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-                // 마커를 생성합니다
-                let marker = new kakao.maps.Marker({
-                    map: map, // 마커를 표시할 지도
-                    position: new kakao.maps.LatLng(x, y),// 마커를 표시할 위치
-                    title : name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                    image : markerImage // 마커 이미지
-                });
-
-            }
-        }
-    });
-}
-
-
-function plus(name) { //이름을 받는 함수
-    $.ajax({ //like값을 올리기
-        type: 'POST',
-        url: '/api/like_button',
-        data: {name_give: name}, // 이름을 건네준다.
-        success: function (response) {
-            alert(response['msg']);
-            window.location.reload() // 새로고침
-        }
-    });
-}
-
-function clickPlaceMarker(title, lat, lng, address) {
-    $('#info-place-name').text(title)
-    $('#info-place-address').text(address)
-    $('#info-place-lng').val(lng)
-    $('#info-place-lat').val(lat)
-
-    $('#place-info').show();
-    $('#place-list').hide();
-
-    showReview();
-}
-
-function makeReview() {
-    // let name = $('#name').val()
-    let name = $('#info-place-name').text()
-    let review = $('#review').val()
-    let lng = $('#info-place-lng').val()
-    let lat = $('#info-place-lat').val()
-    // let rating = $('#rating').val()
-    let like = 0 ///////////////////////////////////
-
-    $.ajax({
-        type: "POST",
-        url: "/placereview",
-        data: {name_give: name, review_give: review, rating_give: like, 'lat_give': lat, 'lng_give': lng},
-        success: function (response) {
-            alert(response["msg"]);
-            window.location.reload();
-        }
-    })
-}
-
-function showReview() {
-    let name = $('#info-place-name').text()
-    $('#review-box').empty();
-    $.ajax({
-        type: "GET",
-        url: `/placereview?name=${name}`,
-        data: {},
-        success: function (response) {
-            let reviews = response['all_reviews']
-            for (let i = 0; i < reviews.length; i++) {
-                // let name = reviews[i]['name']
-                let review = reviews[i]['review']
-                // let rating = reviews[i]['rating']
-
-                let temp_html = `<li class="list-group-item">${review}</li>`
-                $('#review-box').append(temp_html)
-
-                /*
-                * */
-            }
-        }
-    })
-}
-
-function search_place(){
-     $("#hello").hide() // 추천목록 숨기기
 }
