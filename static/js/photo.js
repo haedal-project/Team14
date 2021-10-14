855cfbd91a55624ca6739c4d7a916d30ea801fd7
Yu_Jehyeop <fbwpguq@gmail.com>
2021-10-14 14:27:11 +0900

    업로드 선택 취소(해당 미리보기 이미지 클릭)

--- static/js/photo.js
+++ static/js/photo.js
@@ -5,20 +5,33 @@ $(document).ready(function () {
     $("#file").on("change", handleImgsFilesSelect);
 })
 
+
 function upload_menue(){
     $('#place-list').hide();
     $('#photo-place-test').show();
 }
+
 
 function clickPlace(lat, lng){
     $('#click-place-lat').val(lat);
     $('#click-place-lng').val(lng);
 }
 
+
+function fileUploadAction(){
+    console.log("fileUploadAction");
+    $("#file").trigger('click');
+}
+
+
 function handleImgsFilesSelect(e){
+    sel_files = []
+    $(".images").empty()
+
     let files = e.target.files;
     let filesArr = Array.prototype.slice.call(files);
 
+    let index = 0;
     filesArr.forEach(function (f){
         if(!f.type.match("image.*")) {
             alert("확장자는 이미지 확장자만 가능합니다.")
@@ -28,18 +41,29 @@ function handleImgsFilesSelect(e){
 
         let reader = new FileReader();
         reader.onload = function(e){
-            let img_html = "<span class=\"image-card\"><img src=\""+e.target.result + "\"/></span>";
-            $(".images").append(img_html);
+            let html = "<a href=\"javascript:void(0);\" onclick=\"deleteImageAction("+index+")\" id=\"img_id_"+index+"\"><span class=\"image-card\"><img src=\""+e.target.result + "\" data-file='"+f.name+"' class='sleProductFile' title='Click to remove'></span>";
+            $(".images").append(html);
+            index++;
+
+            // let img_html = "<span class=\"image-card\"><img src=\""+e.target.result + "\"/></span>";
+            // $(".images").append(img_html);
         }
         reader.readAsDataURL(f);
     })
 }
 
-function uploadphoto(){
+function deleteImageAction(index){
+    sel_files.splice(index,1);
+    let img_id = "#img_id_"+ index;
+    $(img_id).remove();
+
+}
+
+function uploadphoto() {
     let lat = $('#click-place-lat').val();
     let lng = $('#click-place-lng').val();
 
-    let file = $('#file')[0].files[0]
+    let file = $('#file')[0].files
     let form_data = new FormData()
 
     form_data.append("file_give", file)
